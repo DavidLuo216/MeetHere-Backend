03e420f4b9fbbf26c163dc0ac51844fa0400bf9c
Lu Wen Qing <david1113531523@gmail.com>
2019-12-18 23:00:28 +0800

    新建新闻评论类

---
+++ src/main/java/cn/ecnuer996/meetHereBackend/dao/CommentMapper.java
@@ -0,0 +1,17 @@
+package cn.ecnuer996.meetHereBackend.dao;
+
+import cn.ecnuer996.meetHereBackend.model.Comment;
+
+public interface CommentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Comment record);
+
+    int insertSelective(Comment record);
+
+    Comment selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Comment record);
+
+    int updateByPrimaryKey(Comment record);
+}
---
+++ src/main/java/cn/ecnuer996/meetHereBackend/model/Comment.java
@@ -0,0 +1,53 @@
+package cn.ecnuer996.meetHereBackend.model;
+
+public class Comment {
+    private Integer id;
+
+    private String type;
+
+    private Integer userId;
+
+    private String content;
+
+    private Integer link;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getLink() {
+        return link;
+    }
+
+    public void setLink(Integer link) {
+        this.link = link;
+    }
+}
