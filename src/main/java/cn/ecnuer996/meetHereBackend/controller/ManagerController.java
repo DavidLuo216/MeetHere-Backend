296be9407b35bbcf998529a626dfc354fdd4b2f3
Lu Wen Qing <david1113531523@gmail.com>
2019-12-30 21:41:39 +0800

    统一管理员相关api的返回格式（使用JsonResult类返回）

--- src/main/java/cn/ecnuer996/meetHereBackend/controller/ManagerController.java
+++ src/main/java/cn/ecnuer996/meetHereBackend/controller/ManagerController.java
@@ -7,6 +7,7 @@ import cn.ecnuer996.meetHereBackend.service.ManagerService;
 import cn.ecnuer996.meetHereBackend.service.UserAuthService;
 import cn.ecnuer996.meetHereBackend.service.UserService;
 import cn.ecnuer996.meetHereBackend.util.FilePathUtil;
+import cn.ecnuer996.meetHereBackend.util.JsonResult;
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -16,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author LuoChengLing
@@ -48,49 +51,37 @@ public class ManagerController {
 
     @ApiOperation("管理员登录接口")
     @PostMapping("/manager-sign-in")
-    public JSONObject logIn(@RequestBody JSONObject request){
-        JSONObject response=new JSONObject();
-        String name=request.getString("name");
-        String password=request.getString("password");
+    public JsonResult logIn(@RequestBody Manager managerParam){
+        String name=managerParam.getName();
+        String password=managerParam.getAvatar();
         Manager manager=managerService.getManagerByName(name);
         if(manager==null){
-            response.put("code",500);
-            response.put("message","不存在的管理员");
+            return new JsonResult(JsonResult.NOT_FOUND,"不存在的管理员");
         }else if(!manager.getPassword().equals(password)){
-            response.put("code",400);
-            response.put("message","密码错误");
+            return new JsonResult(JsonResult.FAIL,"密码错误");
         }else{
-            JSONObject result=new JSONObject();
+            Map<String,Object> result=new HashMap<>(3);
             result.put("name",manager.getName());
             result.put("avatar", FilePathUtil.URL_MANAGER_AVATAR_PREFIX+manager.getAvatar());
             result.put("id",manager.getId());
-            response.put("result",result);
-            response.put("code",200);
-            response.put("message","登录成功");
+            return new JsonResult(result,"登录成功");
         }
-        return response;
     }
 
     @ApiOperation("获取申请找回密码的用户接口")
     @GetMapping("/get-forget-users")
-    public JSONObject getForgetUsers() {
-        JSONObject response=new JSONObject();
+    public JsonResult getForgetUsers() {
         ArrayList<UserAuth> userAuths = userAuthService.getForgetUserAuths();
         ArrayList<User> users = new ArrayList<>();
         for(UserAuth userAuth : userAuths) {
             users.add(userService.getUserByName(userAuth.getIdentifier()));
         }
         if(users.size() == 0) {
-            response.put("code", 404);
-            response.put("message", "列表为空");
-            response.put("result", null);
+            return new JsonResult(JsonResult.NOT_FOUND,"列表为空");
         }
         else {
-            response.put("code", 200);
-            response.put("message", "获取成功");
-            response.put("result", users);
+            return new JsonResult(users);
         }
-        return response;
     }
 
     @ApiOperation("同意找回密码接口")
@@ -98,13 +89,10 @@ public class ManagerController {
     @ApiImplicitParams({
         @ApiImplicitParam(name = "username", value = "用户名", required = true)
     })
-    public JSONObject acceptRediscover(
+    public JsonResult acceptRediscover(
         @RequestParam("username")String username) {
-        JSONObject response=new JSONObject();
         userAuthService.acceptRediscover(username);
-        response.put("code", 200);
-        response.put("message", "修改成功");
-        return response;
+        return new JsonResult();
     }
 
     @ApiOperation("拒绝找回密码接口")
@@ -112,13 +100,10 @@ public class ManagerController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名", required = true)
     })
-    public JSONObject refuseRediscover(
+    public JsonResult refuseRediscover(
             @RequestParam("username")String username) {
-        JSONObject response=new JSONObject();
         userAuthService.refuseRediscover(username);
-        response.put("code", 200);
-        response.put("message", "拒绝成功");
-        return response;
+        return new JsonResult();
     }
 
 }
