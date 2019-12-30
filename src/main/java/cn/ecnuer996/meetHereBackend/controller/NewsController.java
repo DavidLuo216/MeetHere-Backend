d9ae41d77d0a324001bb143a50d89cc38e94ec15
Lu Wen Qing <david1113531523@gmail.com>
2019-12-30 21:51:31 +0800

    统一新闻相关api的返回格式（使用JsonResult类返回）

--- src/main/java/cn/ecnuer996/meetHereBackend/controller/NewsController.java
+++ src/main/java/cn/ecnuer996/meetHereBackend/controller/NewsController.java
@@ -4,6 +4,7 @@ import cn.ecnuer996.meetHereBackend.model.News;
 import cn.ecnuer996.meetHereBackend.model.NewsImageKey;
 import cn.ecnuer996.meetHereBackend.service.NewsService;
 import cn.ecnuer996.meetHereBackend.util.FilePathUtil;
+import cn.ecnuer996.meetHereBackend.util.JsonResult;
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -18,7 +19,9 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -36,22 +39,18 @@ public class NewsController {
     @ApiImplicitParams({ @ApiImplicitParam(name = "segment", value = "每页条数", required = true),
                          @ApiImplicitParam(name = "page", value = "待查询的页号", required = true)})
     @GetMapping(value="/all-news")
-    public JSONObject getAllNews(@RequestParam("segment")Integer segment,
-                                   @RequestParam("page")Integer page){
+    public JsonResult getAllNews(@RequestParam("segment")Integer segment,
+                                 @RequestParam("page")Integer page){
         ArrayList<News> preNews = newsService.getAllNews();
         int numOfPages = Math.max((int) Math.ceil(preNews.size() / (double) segment), 1);
         ArrayList<News> news = new ArrayList<>();
         for(int i = Math.max(page * segment,0); i < Math.min(page * segment + segment, preNews.size()); ++i){
             news.add(preNews.get(i));
         }
-        JSONObject response = new JSONObject();
-        JSONObject result=new JSONObject();
-        response.put("code",200);
-        response.put("messages","查询成功");
+        Map<String,Object> result=new HashMap<>(2);
         result.put("numOfPages",numOfPages);
         result.put("newsList",news);
-        response.put("result",result);
-        return response;
+        return new JsonResult(result);
     }
 
     @ApiOperation("新增新闻")
@@ -60,12 +59,11 @@ public class NewsController {
                          @ApiImplicitParam(name = "title", value = "标题", required = true),
                          @ApiImplicitParam(name = "content", value = "内容", required = true)})
     @PostMapping(value="/add-news")
-    public JSONObject addNews(@RequestParam("managerId")Integer managerId,
+    public JsonResult addNews(@RequestParam("managerId")Integer managerId,
                               @RequestParam("time")String time,
                               @RequestParam("title")String title,
                               @RequestParam("content")String content,
                               @RequestParam("images")MultipartFile[] images){
-        JSONObject response = new JSONObject();
         News news = new News();
         news.setManagerId(managerId);
         try {
@@ -78,9 +76,7 @@ public class NewsController {
         newsService.addNews(news);
         News newsInserted=newsService.getNewsByTitle(title);
         if(newsInserted==null){
-            response.put("code",500);
-            response.put("message","插入数据库失败");
-            return response;
+            return new JsonResult(JsonResult.FAIL,"插入数据库失败");
         }
         int newsId=newsInserted.getId();
         for(MultipartFile image:images){
@@ -91,21 +87,16 @@ public class NewsController {
                 image.transferTo(new File(destFile));
             } catch (IOException e) {
                 e.printStackTrace();
-                response.put("code",500);
-                response.put("message","保存图片失败");
-                return response;
+                return new JsonResult(JsonResult.FAIL,"保存图片失败");
             }
             NewsImageKey newsImageKey=new NewsImageKey();
             newsImageKey.setNewsId(newsId);
             newsImageKey.setImage(destFile);
             newsService.addNewsImage(newsImageKey);
         }
-        JSONObject result=new JSONObject();
+        Map<String,Object> result=new HashMap<>(1);
         result.put("id",newsId);
-        response.put("result",result);
-        response.put("code",200);
-        response.put("message","新增成功");
-        return response;
+        return new JsonResult(result,"新增成功");
     }
 
     @ApiOperation("用id获取单个新闻")
@@ -113,20 +104,15 @@ public class NewsController {
             @ApiImplicitParam(name = "newsId",value = "新闻ID", required = true)
     })
     @GetMapping(value = "/news")
-    public JSONObject getNews(@RequestParam int newsId){
-        JSONObject response=new JSONObject();
-        JSONObject result=new JSONObject();
+    public JsonResult getNews(@RequestParam int newsId){
         News news=newsService.getSingleNews(newsId);
         if(news==null){
-            response.put("code",500);
-            response.put("message","请求失败");
+            return new JsonResult(JsonResult.FAIL,"请求失败");
         }else{
+            Map<String,Object> result=new HashMap<>(1);
             result.put("news",news);
-            response.put("result",result);
-            response.put("code",200);
-            response.put("message","请求成功");
+            return new JsonResult(result);
         }
-        return response;
     }
 
     @ApiOperation("获取单个新闻的所有图片")
@@ -134,19 +120,14 @@ public class NewsController {
             @ApiImplicitParam(name = "newsId",value = "新闻ID", required = true)
     })
     @GetMapping(value = "/news-images")
-    public JSONObject getNewsImages(@RequestParam int newsId){
-        JSONObject response=new JSONObject();
-        JSONObject result=new JSONObject();
+    public JsonResult getNewsImages(@RequestParam int newsId){
         List<String> images=newsService.getNewsImages(newsId);
         if(images==null){
-            response.put("code",500);
-            response.put("message","请求失败");
+            return new JsonResult(JsonResult.FAIL,"请求失败");
         }else{
+            Map<String,Object> result=new HashMap<>(1);
             result.put("images",images);
-            response.put("result",result);
-            response.put("code",200);
-            response.put("message","请求成功");
+            return new JsonResult(result);
         }
-        return response;
     }
 }
