1a82427bfb07d6d73432e88c62aa2012228ee507
Lu Wen Qing <david1113531523@gmail.com>
2020-01-02 19:45:07 +0800

    格式化ManagerController代码（覆盖度计算奇怪的bug）

--- src/main/java/cn/ecnuer996/meetHereBackend/controller/ManagerController.java
+++ src/main/java/cn/ecnuer996/meetHereBackend/controller/ManagerController.java
@@ -37,17 +37,17 @@ public class ManagerController {
     private ReservationService reservationService;
 
     @Autowired
-    public void setUserService(UserService userService){
+    public void setUserService(UserService userService) {
         this.userService = userService;
     }
 
     @Autowired
-    public void setUserService(SiteService siteService){
+    public void setUserService(SiteService siteService) {
         this.siteService = siteService;
     }
 
     @Autowired
-    public void setVenueService(VenueService venueService){
+    public void setVenueService(VenueService venueService) {
         this.venueService = venueService;
     }
 
@@ -62,29 +62,29 @@ public class ManagerController {
     }
 
     @Autowired
-    public void setReservationService(ReservationService reservationService){
+    public void setReservationService(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
-    /* Change Date To String Begin */
     private String DateToDate(Date date) {
         return new SimpleDateFormat("yyyy-MM-dd").format(date);
     }
+
     private String DateToTime(Date date) {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
     }
-    /* Change Date To String Finish */
 
     @ApiOperation("管理员详情")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "managerId", value = "管理员ID", required = true)
+    })
     @GetMapping("/manager")
-    @ApiImplicitParams({@ApiImplicitParam(name = "managerId",value = "管理员ID", required = true)})
     public JsonResult getManagerDetail(@RequestParam int managerId) {
         Manager manager = managerService.getManager(managerId);
-        if(manager == null) {
+        if (manager == null) {
             return new JsonResult(JsonResult.NOT_FOUND, "管理员不存在");
-        }
-        else {
-            Map<String, Object> result=new HashMap<>(1);
+        } else {
+            Map<String, Object> result = new HashMap<>(1);
             result.put("manager", manager);
             return new JsonResult(result);
         }
@@ -92,22 +92,20 @@ public class ManagerController {
 
     @ApiOperation("管理员登录接口")
     @PostMapping("/manager-sign-in")
-    public JsonResult logIn(@RequestBody Manager managerParam){
-        String name=managerParam.getName();
-        String password=managerParam.getPassword();
-        Manager manager=managerService.getManagerByName(name);
-        if(manager==null){
-            return new JsonResult(JsonResult.NOT_FOUND,"不存在的管理员");
-        }
-        else if(!manager.getPassword().equals(password)){
-            return new JsonResult(JsonResult.FAIL,"密码错误");
-        }
-        else {
-            Map<String,Object> result=new HashMap<>(3);
-            result.put("name",manager.getName());
-            result.put("avatar", FilePathUtil.URL_MANAGER_AVATAR_PREFIX+manager.getAvatar());
-            result.put("id",manager.getId());
-            return new JsonResult(result,"登录成功");
+    public JsonResult logIn(@RequestBody Manager managerParam) {
+        String name = managerParam.getName();
+        String password = managerParam.getPassword();
+        Manager manager = managerService.getManagerByName(name);
+        if (manager == null) {
+            return new JsonResult(JsonResult.NOT_FOUND, "不存在的管理员");
+        } else if (!manager.getPassword().equals(password)) {
+            return new JsonResult(JsonResult.FAIL, "密码错误");
+        } else {
+            Map<String, Object> result = new HashMap<>(3);
+            result.put("name", manager.getName());
+            result.put("avatar", FilePathUtil.URL_MANAGER_AVATAR_PREFIX + manager.getAvatar());
+            result.put("id", manager.getId());
+            return new JsonResult(result, "登录成功");
         }
     }
 
@@ -116,54 +114,55 @@ public class ManagerController {
     public JsonResult getForgetUsers() {
         ArrayList<UserAuth> userAuths = userAuthService.getForgetUserAuths();
         ArrayList<User> users = new ArrayList<>();
-        for(UserAuth userAuth : userAuths) {
+        for (UserAuth userAuth : userAuths) {
             users.add(userService.getUserByName(userAuth.getIdentifier()));
         }
-        if(users.size() == 0) {
-            return new JsonResult(JsonResult.NOT_FOUND,"列表为空");
-        }
-        else {
+        if (users.size() == 0) {
+            return new JsonResult(JsonResult.NOT_FOUND, "列表为空");
+        } else {
             return new JsonResult(users);
         }
     }
 
     @ApiOperation("同意找回密码接口")
-    @GetMapping("/accept-rediscover")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名", required = true)
     })
+    @GetMapping("/accept-rediscover")
     public JsonResult acceptRediscover(
-            @RequestParam("username")String username) {
+            @RequestParam("username") String username) {
         userAuthService.acceptRediscover(username);
         return new JsonResult();
     }
 
     @ApiOperation("拒绝找回密码接口")
-    @GetMapping("/refuse-rediscover")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名", required = true)
     })
+    @GetMapping("/refuse-rediscover")
     public JsonResult refuseRediscover(
-            @RequestParam("username")String username) {
+            @RequestParam("username") String username) {
         userAuthService.refuseRediscover(username);
         return new JsonResult();
     }
 
     @ApiOperation("分页查询进行中订单列表接口")
-    @ApiImplicitParams({ @ApiImplicitParam(name = "segment", value = "每页条数", required = true),
-                         @ApiImplicitParam(name = "page", value = "待查询的页号", required = true)})
-    @GetMapping(value="/going-orders")
-    public JsonResult searchOrders(@RequestParam("segment")Integer segment,
-                                   @RequestParam("page")Integer page) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "segment", value = "每页条数", required = true),
+            @ApiImplicitParam(name = "page", value = "待查询的页号", required = true)
+    })
+    @GetMapping(value = "/going-orders")
+    public JsonResult searchOrders(@RequestParam("segment") Integer segment,
+                                   @RequestParam("page") Integer page) {
         List<Reservation> pre_reservations = reservationService.getGoingReservation();
         int num_of_pages = Math.max((int) Math.ceil(pre_reservations.size() / (double) segment), 1);
         List<Reservation> reservations = new ArrayList<>();
-        for(int i = Math.max(page * segment,0); i < Math.min(page * segment + segment, pre_reservations.size()); ++i){
+        for (int i = Math.max(page * segment, 0); i < Math.min(page * segment + segment, pre_reservations.size()); ++i) {
             reservations.add(pre_reservations.get(i));
         }
         ArrayList<ReservationDetail> details = new ArrayList<>();
         int len = reservations.size();
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             Reservation reservation = reservations.get(i);
             ReservationDetail item = new ReservationDetail();
             Site site = siteService.getSiteById(reservation.getSiteId());
@@ -180,10 +179,10 @@ public class ManagerController {
             /* Calculate Element Value Finish */
             details.add(item);
         }
-        Map<String,Object> result=new HashMap<>(2);
-        result.put("details",details);
-        result.put("num_of_pages",num_of_pages);
-        return new JsonResult(result,"查询成功");
+        Map<String, Object> result = new HashMap<>(2);
+        result.put("details", details);
+        result.put("num_of_pages", num_of_pages);
+        return new JsonResult(result, "查询成功");
     }
 
 }
