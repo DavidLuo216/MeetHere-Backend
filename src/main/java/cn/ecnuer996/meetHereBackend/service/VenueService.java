5aae594df5d3034bd45baed8a34c6110ba73b2fc
Lu Wen Qing <david1113531523@gmail.com>
2020-01-03 15:32:37 +0800

    完成VenueService剩余测试

--- src/main/java/cn/ecnuer996/meetHereBackend/service/VenueService.java
+++ src/main/java/cn/ecnuer996/meetHereBackend/service/VenueService.java
@@ -33,14 +33,17 @@ public class VenueService {
     public void setVenueDao(VenueMapper venueDao) {
         this.venueDao = venueDao;
     }
+
     @Autowired
     public void setSiteDao(SiteMapper siteDao) {
         this.siteDao = siteDao;
     }
+
     @Autowired
     public void setVenueImageDao(VenueImageMapper venueImageDao) {
         this.venueImageDao = venueImageDao;
     }
+
     @Autowired
     public void setReservationDao(ReservationMapper reservationDao) {
         this.reservationDao = reservationDao;
@@ -49,182 +52,181 @@ public class VenueService {
     /**
      * 预定的最小时间单元，单位为分钟
      */
-    final static int ReservationPeriod=30;
+    final static int ReservationPeriod = 30;
 
     public Venue getVenueById(int id) {
         return venueDao.selectByPrimaryKey(id);
     }
 
-    public ArrayList<Venue> getVenueByName(String name){
+    public ArrayList<Venue> getVenueByName(String name) {
         return venueDao.selectByVenueName(name);
     }
 
-    public ArrayList<Site> getSiteByVenueId(int venue_id){
-        ArrayList<Site> sites=siteDao.selectByVenueId(venue_id);
-        for(Site site:sites){
-            site.setImage(FilePathUtil.URL_SITE_IMAGE_PREFIX +site.getImage());
+    public ArrayList<Site> getSiteByVenueId(int venue_id) {
+        ArrayList<Site> sites = siteDao.selectByVenueId(venue_id);
+        for (Site site : sites) {
+            site.setImage(FilePathUtil.URL_SITE_IMAGE_PREFIX + site.getImage());
         }
         return sites;
     }
 
-    public ArrayList<VenueInList> getAllVenues(){
-        List<Venue> rawVenues=venueDao.selectAllVenues();
-        List<VenueInList> listVenues=new ArrayList<>();
-        SimpleDateFormat formatter=new SimpleDateFormat("HH:mm");
+    public ArrayList<VenueInList> getAllVenues() {
+        List<Venue> rawVenues = venueDao.selectAllVenues();
+        List<VenueInList> listVenues = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
         formatter.setTimeZone(TimeZone.getTimeZone("GMT+0"));
-        for(Venue venue:rawVenues){
-            VenueInList venueItem=new VenueInList();
-            venueItem.id=venue.getId();
-            venueItem.name=venue.getName();
-            venueItem.address=venue.getAddress();
-            venueItem.beginTime=venue.getBeginTime();
-            venueItem.endTime=venue.getEndTime();
-            venueItem.cover=FilePathUtil.URL_VENUE_COVER_PREFIX+venueImageDao.getVenueCoverByVenueId(venue.getId());
+        for (Venue venue : rawVenues) {
+            VenueInList venueItem = new VenueInList();
+            venueItem.id = venue.getId();
+            venueItem.name = venue.getName();
+            venueItem.address = venue.getAddress();
+            venueItem.beginTime = venue.getBeginTime();
+            venueItem.endTime = venue.getEndTime();
+            venueItem.cover = FilePathUtil.URL_VENUE_COVER_PREFIX + venueImageDao.getVenueCoverByVenueId(venue.getId());
             listVenues.add(venueItem);
         }
         return (ArrayList<VenueInList>) listVenues;
     }
 
-    public Map<String, Object> getVenueDetail(int venue_id){
-        Venue venue=venueDao.selectByPrimaryKey(venue_id);
-        List<String> images=venueImageDao.getVenueImagesByVenueId(venue_id);
-        for(int i=0;i<images.size();++i){
-            images.set(i,FilePathUtil.URL_VENUE_COVER_PREFIX+images.get(i));
+    public Map<String, Object> getVenueDetail(int venue_id) {
+        Venue venue = venueDao.selectByPrimaryKey(venue_id);
+        List<String> images = venueImageDao.getVenueImagesByVenueId(venue_id);
+        for (int i = 0; i < images.size(); ++i) {
+            images.set(i, FilePathUtil.URL_VENUE_COVER_PREFIX + images.get(i));
         }
-        Map<String,Object> venueDetail=new HashMap<>(9);
-        SimpleDateFormat formatter=new SimpleDateFormat("HH:mm");
-        venueDetail.put("id",venue.getId());
-        venueDetail.put("name",venue.getName());
-        venueDetail.put("address",venue.getAddress());
-        venueDetail.put("introduction",venue.getIntroduction());
-        venueDetail.put("phone",venue.getPhone());
-        venueDetail.put("beginTime",formatter.format(venue.getBeginTime()));
-        venueDetail.put("endTime",formatter.format(venue.getEndTime()));
-        venueDetail.put("images",images);
-        ArrayList sites=getSiteByVenueId(venue_id);
-        venueDetail.put("sites",sites);
+        Map<String, Object> venueDetail = new HashMap<>(9);
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        venueDetail.put("id", venue.getId());
+        venueDetail.put("name", venue.getName());
+        venueDetail.put("address", venue.getAddress());
+        venueDetail.put("introduction", venue.getIntroduction());
+        venueDetail.put("phone", venue.getPhone());
+        venueDetail.put("beginTime", formatter.format(venue.getBeginTime()));
+        venueDetail.put("endTime", formatter.format(venue.getEndTime()));
+        venueDetail.put("images", images);
+        ArrayList sites = getSiteByVenueId(venue_id);
+        venueDetail.put("sites", sites);
         return venueDetail;
     }
 
     public Map<String, Object> getSiteTimes(int siteId, String date) throws ParseException {
-        SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd");
-        //mysql时区问题尚未解决，只能将日期按照GMT+0时区解析
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT+0"));
-        Date realDate=formatter.parse(date);
-        List<Reservation> reservations=reservationDao.selectBySiteIdAndDate(siteId,realDate);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date realDate = formatter.parse(date);
+        List<Reservation> reservations = reservationDao.selectBySiteIdAndDate(siteId, realDate);
 
-        Site site=siteDao.selectByPrimaryKey(siteId);
-        Venue venue=venueDao.selectByPrimaryKey(site.getVenueId());
+        Site site = siteDao.selectByPrimaryKey(siteId);
+        Venue venue = venueDao.selectByPrimaryKey(site.getVenueId());
 
-        int beginMinutes=(int)((((venue.getBeginTime().getTime())/60000)%1440));
-        int endMinutes=(int)((((venue.getEndTime().getTime())/60000)%1440));
-        int beginId=beginMinutes/ReservationPeriod;
-        int endId=endMinutes/ReservationPeriod;
+        int beginMinutes = (int) ((((venue.getBeginTime().getTime()) / 60000) % 1440));
+        int endMinutes = (int) ((((venue.getEndTime().getTime()) / 60000) % 1440));
+        int beginId = beginMinutes / ReservationPeriod;
+        int endId = endMinutes / ReservationPeriod;
 
-        int[] bookableList=new int[48];
+        int[] bookableList = new int[48];
         Arrays.fill(bookableList, -1);
-        for(int i=beginId;i<endId;++i){
-            bookableList[i]=0;
+        for (int i = beginId; i < endId; ++i) {
+            bookableList[i] = 0;
         }
-        for(Reservation r:reservations){
-            for(int i=r.getBeginTime();i<=r.getEndTime();++i){
+        for (Reservation r : reservations) {
+            for (int i = r.getBeginTime(); i <= r.getEndTime(); ++i) {
                 // 标记此时段已被预约
-                bookableList[i]=1;
+                bookableList[i] = 1;
             }
         }
-        Map<String,Object> ret=new HashMap<>(2);
-        List<Map<String,Object>> siteTimes=new ArrayList<>();
-        for(int i=0;i<bookableList.length;++i){
-            Map<String,Object> siteTime=new HashMap<>(3);
-            siteTime.put("period",simplePrintPeriod(i));
-            siteTime.put("bookable",bookableList[i]);
-            siteTime.put("periodId",i);
+        Map<String, Object> ret = new HashMap<>(2);
+        List<Map<String, Object>> siteTimes = new ArrayList<>();
+        for (int i = 0; i < bookableList.length; ++i) {
+            Map<String, Object> siteTime = new HashMap<>(3);
+            siteTime.put("period", simplePrintPeriod(i));
+            siteTime.put("bookable", bookableList[i]);
+            siteTime.put("periodId", i);
             siteTimes.add(siteTime);
         }
-        ret.put("siteTimes",siteTimes);
-        ret.put("bookDate",date);
+        ret.put("siteTimes", siteTimes);
+        ret.put("bookDate", date);
         return ret;
     }
 
     /**
      * 根据要预定的场地ID和日期返回一个布尔值列表表示各时段是否可预约
+     *
      * @param siteId
      * @param date
      * @return
      */
-    public int[] getReservationsBySiteIdAndDate(int siteId,Date date){
-        List<Reservation> reservations=reservationDao.selectBySiteIdAndDate(siteId,date);
-        Site site=siteDao.selectByPrimaryKey(siteId);
-        Venue venue=venueDao.selectByPrimaryKey(site.getVenueId());
+    public int[] getReservationsBySiteIdAndDate(int siteId, Date date) {
+        List<Reservation> reservations = reservationDao.selectBySiteIdAndDate(siteId, date);
+        Site site = siteDao.selectByPrimaryKey(siteId);
+        Venue venue = venueDao.selectByPrimaryKey(site.getVenueId());
 
-        int beginMinutes=(int)((((venue.getBeginTime().getTime())/60000)%1440));
-        int endMinutes=(int)((((venue.getEndTime().getTime())/60000)%1440));
-        int beginId=beginMinutes/ReservationPeriod;
-        int endId=endMinutes/ReservationPeriod;
+        int beginMinutes = (int) ((((venue.getBeginTime().getTime()) / 60000) % 1440));
+        int endMinutes = (int) ((((venue.getEndTime().getTime()) / 60000) % 1440));
+        int beginId = beginMinutes / ReservationPeriod;
+        int endId = endMinutes / ReservationPeriod;
 
-        int[] bookableList=new int[48];
-        for(int i=0;i<bookableList.length;++i){
-            bookableList[i]=-1;
+        int[] bookableList = new int[48];
+        for (int i = 0; i < bookableList.length; ++i) {
+            bookableList[i] = -1;
         }
-        for(int i=beginId;i<endId;++i){
-            bookableList[i]=0;
+        for (int i = beginId; i < endId; ++i) {
+            bookableList[i] = 0;
         }
-        for(Reservation r:reservations){
-            for(int i=r.getBeginTime();i<r.getEndTime();++i){
+        for (Reservation r : reservations) {
+            for (int i = r.getBeginTime(); i < r.getEndTime(); ++i) {
                 // 标记此时段已被预约
-                bookableList[i]=1;
+                bookableList[i] = 1;
             }
         }
         return bookableList;
     }
 
-    public Float calculatePrice(int siteId,int periodNum){
-        return siteDao.selectByPrimaryKey(siteId).getPrice()*periodNum/2;
+    public Float calculatePrice(int siteId, int periodNum) {
+        return siteDao.selectByPrimaryKey(siteId).getPrice() * periodNum / 2;
     }
 
-    public int addReservation(Reservation reservation){
+    public int addReservation(Reservation reservation) {
         return reservationDao.insertSelective(reservation);
     }
 
-    public ReservationDetail getLatestReservation(int userId){
-        ReservationDetail reservationDetail=new ReservationDetail();
-        Reservation latestReservation=reservationDao.selectLatestReservationByUserId(userId);
-        SimpleDateFormat dateTimeFormatter=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public ReservationDetail getLatestReservation(int userId) {
+        ReservationDetail reservationDetail = new ReservationDetail();
+        Reservation latestReservation = reservationDao.selectLatestReservationByUserId(userId);
+        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         //mysql时区问题尚未解决，只能将日期按照GMT+8时区解析
         dateTimeFormatter.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        SimpleDateFormat dateFormatter=new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
         dateFormatter.setTimeZone(TimeZone.getTimeZone("GMT+0"));
 
-        Site site=siteDao.selectByPrimaryKey(latestReservation.getSiteId());
-        Venue venue=venueDao.selectByPrimaryKey(site.getVenueId());
+        Site site = siteDao.selectByPrimaryKey(latestReservation.getSiteId());
+        Venue venue = venueDao.selectByPrimaryKey(site.getVenueId());
 
         reservationDetail.setId(latestReservation.getId());
         reservationDetail.setVenueName(venue.getName());
         reservationDetail.setSiteName(site.getName());
-        reservationDetail.setSiteImage(FilePathUtil.URL_SITE_IMAGE_PREFIX+site.getImage());
+        reservationDetail.setSiteImage(FilePathUtil.URL_SITE_IMAGE_PREFIX + site.getImage());
         reservationDetail.setBookTime(dateTimeFormatter.format(latestReservation.getBookTime()));
         reservationDetail.setCost(latestReservation.getCost());
         reservationDetail.setReserveDate(dateFormatter.format(latestReservation.getDate()));
-        reservationDetail.setState(ReservationState.states.get(latestReservation.getSiteId()));
+        reservationDetail.setState(ReservationState.states.get(latestReservation.getState()));
 
         reservationDetail.setBeginTime(simplePrintPeriod(latestReservation.getBeginTime()));
-        reservationDetail.setEndTime(simplePrintPeriod(latestReservation.getEndTime()+1));
+        reservationDetail.setEndTime(simplePrintPeriod(latestReservation.getEndTime() + 1));
 
         return reservationDetail;
     }
 
-    public String simplePrintPeriod(int periodId){
-        if(periodId%2==0){
-            if(periodId/2<10) {
-                return "0"+(periodId/2)+":00";
+    public String simplePrintPeriod(int periodId) {
+        if (periodId % 2 == 0) {
+            if (periodId / 2 < 10) {
+                return "0" + (periodId / 2) + ":00";
             } else {
-                return (periodId/2)+":00";
+                return (periodId / 2) + ":00";
             }
-        }else{
-            if(periodId/2<10) {
-                return "0"+(periodId/2)+":30";
+        } else {
+            if (periodId / 2 < 10) {
+                return "0" + (periodId / 2) + ":30";
             } else {
-                return (periodId/2)+":30";
+                return (periodId / 2) + ":30";
             }
         }
     }
--- src/test/java/cn/ecnuer996/meetHereBackend/service/VenueServiceTest.java
+++ src/test/java/cn/ecnuer996/meetHereBackend/service/VenueServiceTest.java
@@ -4,8 +4,10 @@ import cn.ecnuer996.meetHereBackend.dao.ReservationMapper;
 import cn.ecnuer996.meetHereBackend.dao.SiteMapper;
 import cn.ecnuer996.meetHereBackend.dao.VenueImageMapper;
 import cn.ecnuer996.meetHereBackend.dao.VenueMapper;
+import cn.ecnuer996.meetHereBackend.model.Reservation;
 import cn.ecnuer996.meetHereBackend.model.Site;
 import cn.ecnuer996.meetHereBackend.model.Venue;
+import cn.ecnuer996.meetHereBackend.transfer.ReservationDetail;
 import cn.ecnuer996.meetHereBackend.transfer.VenueInList;
 import cn.ecnuer996.meetHereBackend.util.FilePathUtil;
 import org.junit.jupiter.api.Test;
@@ -15,9 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -110,22 +110,116 @@ class VenueServiceTest {
     }
 
     @Test
-    void getSiteTimes() {
+    void getSiteTimes() throws ParseException {
+        List<Reservation> reservations=new ArrayList<>();
+        for (int i=0;i<3;++i){
+            Reservation reservation=new Reservation();
+            reservation.setBeginTime(16+i*4);
+            reservation.setEndTime(19+i*4);
+            reservations.add(reservation);
+        }
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+        when(reservationDao.selectBySiteIdAndDate(1,format.parse("2019-12-12"))).thenReturn(reservations);
+        Site site=new Site();
+        site.setVenueId(5);
+        when(siteDao.selectByPrimaryKey(1)).thenReturn(site);
+        Venue venue=new Venue();
+        venue.setId(5);
+        SimpleDateFormat timeFormat=new SimpleDateFormat("HH:mm");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+        venue.setBeginTime(timeFormat.parse("07:00"));
+        venue.setEndTime(timeFormat.parse("19:00"));
+        when(venueDao.selectByPrimaryKey(5)).thenReturn(venue);
+        Map<String,Object> result=venueService.getSiteTimes(1,"2019-12-12");
+        List<Map<String,Object>> siteTimes= (List<Map<String, Object>>) result.get("siteTimes");
+        assertEquals(siteTimes.get(0).get("bookable"),-1);
+        assertEquals(siteTimes.get(13).get("bookable"),-1);
+        assertEquals(siteTimes.get(14).get("bookable"),0);
+        assertEquals(siteTimes.get(37).get("bookable"),0);
+        assertEquals(siteTimes.get(20).get("bookable"),1);
+        assertEquals(siteTimes.get(27).get("bookable"),1);
+        assertEquals(siteTimes.get(38).get("bookable"),-1);
+        assertEquals(siteTimes.get(47).get("bookable"),-1);
     }
 
     @Test
-    void getReservationsBySiteIdAndDate() {
+    void getReservationsBySiteIdAndDate() throws ParseException {
+        List<Reservation> reservations=new ArrayList<>();
+        for (int i=0;i<3;++i){
+            Reservation reservation=new Reservation();
+            reservation.setBeginTime(16+i*4);
+            reservation.setEndTime(19+i*4);
+            reservations.add(reservation);
+        }
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+        when(reservationDao.selectBySiteIdAndDate(1,format.parse("2019-12-12"))).thenReturn(reservations);
+        Site site=new Site();
+        site.setVenueId(5);
+        when(siteDao.selectByPrimaryKey(1)).thenReturn(site);
+        Venue venue=new Venue();
+        venue.setId(5);
+        SimpleDateFormat timeFormat=new SimpleDateFormat("HH:mm");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+        venue.setBeginTime(timeFormat.parse("07:00"));
+        venue.setEndTime(timeFormat.parse("19:00"));
+        when(venueDao.selectByPrimaryKey(5)).thenReturn(venue);
+        int[] bookableList=venueService.getReservationsBySiteIdAndDate(1,format.parse("2019-12-12"));
+        assertEquals(bookableList[0],-1);
+        assertEquals(bookableList[13],-1);
+        assertEquals(bookableList[38],-1);
+        assertEquals(bookableList[47],-1);
+        assertEquals(bookableList[14],0);
+        assertEquals(bookableList[37],0);
+        assertEquals(bookableList[20],1);
+        assertEquals(bookableList[26],1);
     }
 
     @Test
     void calculatePrice() {
+        Site site=new Site();
+        site.setPrice(60f);
+        when(siteDao.selectByPrimaryKey(1)).thenReturn(site);
+        assertEquals(venueService.calculatePrice(1,4),120f);
     }
 
     @Test
     void addReservation() {
+        when(reservationDao.insertSelective(isA(Reservation.class))).thenReturn(1);
+        venueService.addReservation(new Reservation());
+        verify(reservationDao,times(1)).insertSelective(isA(Reservation.class));
     }
 
     @Test
-    void getLatestReservation() {
+    void getLatestReservation() throws ParseException {
+        Reservation reservation=new Reservation();
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+        reservation.setSiteId(3);
+        reservation.setId(12345);
+        reservation.setBookTime(new Date());
+        reservation.setCost(120f);
+        reservation.setDate(format.parse("2019-12-12"));
+        reservation.setState(1);
+        reservation.setBeginTime(14);
+        reservation.setEndTime(16);
+        when(reservationDao.selectLatestReservationByUserId(1)).thenReturn(reservation);
+        Site site=new Site();
+        site.setVenueId(5);
+        site.setName("site name");
+        Venue venue=new Venue();
+        venue.setId(5);
+        venue.setName("venue name");
+        when(siteDao.selectByPrimaryKey(3)).thenReturn(site);
+        when(venueDao.selectByPrimaryKey(5)).thenReturn(venue);
+        ReservationDetail reservationDetail=venueService.getLatestReservation(1);
+        assertEquals(reservationDetail.getBeginTime(),"07:00");
+        assertEquals(reservationDetail.getEndTime(),"08:30");
+    }
+
+    @Test
+    void simplePrintPeriod(){
+        assertEquals(venueService.simplePrintPeriod(18),"09:00");
+        assertEquals(venueService.simplePrintPeriod(19),"09:30");
+        assertEquals(venueService.simplePrintPeriod(22),"11:00");
+        assertEquals(venueService.simplePrintPeriod(23),"11:30");
     }
 }
